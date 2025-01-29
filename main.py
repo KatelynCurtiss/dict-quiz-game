1434e3d8ce10ba76a947d66f9e6be7976f2bc3e2
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-01-29 10:27:54 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+# Katelyn Curtiss 
+# January 29 2025
+# Dictionary Quiz Game
+
+score = 0 
+questions = [
+    {
+        'question': "What year did World War I ___ start?",
+        'answer': "1914"
+    },
+      {
+        'question': "What year did World War II ___ start?",
+        'answer': "1939"
+    },
+      
+      {
+        'question': "How many years did World War II  ___ last?",
+        'answer': "6"
+    },
+    
+    {
+        'question': "In World War II Germany invaded ___ ,which started the war?",
+        'answer': "Poland"
+    },
+    
+    {
+        'question': "What was the deadliest war ___ ?",
+        'answer': "World War II"
+    }
+]
+
+for question in questions:
+    question_text = question.get("question")
+    print(question_text)
+
+    user_answer = input("Enter answer: ")
+    correct_answer = question.get("answer")
+    
+    if user_answer == correct_answer:
+       score = score + 10
+
+print("Ypur score is = ", score)
